2cf212c8a4a4c2555aad22c0539a7438287e39e0
james8888 <james.kdm@gmail.com>
2022-04-20 16:25:46 +0530

    modify the sql

--- fivetran_dbt/dbttrans1.sql
+++ fivetran_dbt/dbttrans1.sql
@@ -1,10 +1,33 @@
-USE SCHEMA "FIVETRAN_DATABASE"."AZURE_BLOB_STORAGE";
-CREATE OR REPLACE VIEW unpacked_json_dbt AS 
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
 select 
 ID,
 _modified modified,
 f.value:name name,
 f.value:email email,
- _FIVETRAN_SYNCED FIVETRAN_SYNCED
-from JSONFT,
-lateral flatten(input => users ) f;
+_FIVETRAN_SYNCED FIVETRAN_SYNCED
+from "FIVETRAN_DATABASE"."AZURE_BLOB_STORAGE"."JSONFT",
+lateral flatten(input => users ) f
+
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
+
--- fivetran_dbt/models/dbttrans.sql
+++ fivetran_dbt/models/dbttrans.sql
@@ -31,29 +31,3 @@ from source_data
 
 -- where id is not null
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-USE SCHEMA "FIVETRAN_DATABASE"."AZURE_BLOB_STORAGE";
-CREATE OR REPLACE VIEW unpacked_json_dbt AS 
-select 
-ID,
-_modified modified,
-f.value:name name,
-f.value:email email,
- _FIVETRAN_SYNCED FIVETRAN_SYNCED
-from "FIVETRAN_DATABASE"."AZURE_BLOB_STORAGE"."JSONFT",
-lateral flatten(input => users ) f;
